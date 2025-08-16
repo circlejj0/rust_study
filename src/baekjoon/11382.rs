7159b514bf149ebf9f2afa3e849981277232154d
WonJeong Kim (김원정) <circlejj@pukyong.ac.kr>
2025-08-17 00:12:04 +0900

    Create 11382.rs

---
+++ src/baekjoon/11382.rs
@@ -0,0 +1,12 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let mut iter = input.split_whitespace().map(|x| x.parse::<u128>().unwrap());
+    let a = iter.next().unwrap();
+    let b = iter.next().unwrap();
+    let c = iter.next().unwrap();
+    println!("{}", a + b + c);
+}
